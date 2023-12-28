75481c57f3949e4981bad396a73660423fa3c8a5
Nathan Wondwossen <nathanplaysmine@gmail.com>
2023-12-28 20:14:00 +0300

    delete product when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProducts.id}">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -92,15 +92,18 @@ cart.forEach((cartItem) => {
     </div>
   `;
 });
-document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
-
+document.querySelector('.js-order-summary')
+.innerHTML = cartSummaryHTML;
 
 document.querySelectorAll('.js-delete-link')
-  .forEach((link)=>{
+.forEach((link) => {
+  link.addEventListener('click', () => {
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
 
-    link.addEventListener('click',()=>{
-      const productId = link.dataset.productId;
-      removeFromCart(productId);
-    })
-
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
+});
